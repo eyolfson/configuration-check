b0747d1df9efab751b6526f10360da33802fa4fe
Jon Eyolfson <jon@eyl.io>
2015-11-30 21:35:05 -0500

    [check] Whitespace improvement

--- src/main.c
+++ src/main.c
@@ -109,7 +109,6 @@ int check_user(const char *check_directory,
 	char user_directory[PATH_MAX];
 	strcpy(user_directory, check_directory);
 	strcat(user_directory, "/user");
-
 	if (!is_directory(check_directory)) {
 		printf("%sCheck requires a 'user' directory%s\n",
 			ANSI_RED, ANSI_RESET);
