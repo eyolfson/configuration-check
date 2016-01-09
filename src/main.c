0bf5a0fb44ba503fb122fed1009206fa4bd31833
Jon Eyolfson <jon@eyl.io>
2016-01-09 17:19:18 -0500

    Update copyright year

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Jonathan Eyolfson
+ * Copyright 2015-2016 Jonathan Eyolfson
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
